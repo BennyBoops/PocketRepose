e63d07fde2d30792ce4f6fea8db6536861fe5aae
BennyBoops <benjgerwitz@gmail.com>
2025-03-30 18:00:28 -0400

    1.21.5 NOT WORKING -Updated version. -Need to fix problems.

--- src/main/java/net/bennyboops/modid/block/ModBlocks.java
+++ src/main/java/net/bennyboops/modid/block/ModBlocks.java
@@ -1,8 +1,6 @@
 package net.bennyboops.modid.block;
 
 import net.bennyboops.modid.PocketRepose;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
@@ -14,44 +12,44 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block SUITCASE = registerBlock("suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.BROWN_WOOL)
+            new SuitcaseBlock(Block.Settings.copy(Blocks.BROWN_WOOL)
                     .sounds(BlockSoundGroup.WOOL)
                     .strength(0.2f)
                     .nonOpaque()
                     .luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block WHITE_SUITCASE = registerBlock("white_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.WHITE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.WHITE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block BLACK_SUITCASE = registerBlock("black_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.BLACK_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.BLACK_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block LIGHT_GRAY_SUITCASE = registerBlock("light_gray_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.LIGHT_GRAY_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.LIGHT_GRAY_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block GRAY_SUITCASE = registerBlock("gray_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.GRAY_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.GRAY_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block RED_SUITCASE = registerBlock("red_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.RED_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.RED_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block ORANGE_SUITCASE = registerBlock("orange_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.ORANGE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.ORANGE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block YELLOW_SUITCASE = registerBlock("yellow_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.YELLOW_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.YELLOW_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block LIME_SUITCASE = registerBlock("lime_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.LIME_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.LIME_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block GREEN_SUITCASE = registerBlock("green_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.GREEN_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.GREEN_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block CYAN_SUITCASE = registerBlock("cyan_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.CYAN_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.CYAN_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block LIGHT_BLUE_SUITCASE = registerBlock("light_blue_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.LIGHT_BLUE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.LIGHT_BLUE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block BLUE_SUITCASE = registerBlock("blue_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.BLUE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.BLUE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block PURPLE_SUITCASE = registerBlock("purple_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.PURPLE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.PURPLE_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block MAGENTA_SUITCASE = registerBlock("magenta_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.MAGENTA_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.MAGENTA_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
     public static final Block PINK_SUITCASE = registerBlock("pink_suitcase",
-            new SuitcaseBlock(FabricBlockSettings.copyOf(Blocks.PINK_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
+            new SuitcaseBlock(Block.Settings.copy(Blocks.PINK_WOOL).sounds(BlockSoundGroup.WOOL).strength(0.2f).nonOpaque().luminance(state -> state.get(SuitcaseBlock.OPEN) ? 8 : 0)));
 
     public static final Block PORTAL = registerBlock("portal",
-            new PocketPortalBlock(FabricBlockSettings.copyOf(Blocks.NETHER_PORTAL)
+            new PocketPortalBlock(Block.Settings.copy(Blocks.NETHER_PORTAL)
                     .sounds(BlockSoundGroup.LODESTONE)
                     .nonOpaque()
                     .luminance(10)
@@ -64,7 +62,7 @@ public class ModBlocks {
     }
     private static Item registerBlockItem(String name, Block block) {
         return Registry.register(Registries.ITEM, new Identifier(PocketRepose.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings()));
+                new BlockItem(block, new Item.Settings()));
     }
     public static void registerModBlocks() {
         PocketRepose.LOGGER.info("Registering mod blocks for" + PocketRepose.MOD_ID);
